4dacc4cccc6a47f8e8d945c53d28e2f9fc697e2d
diplodoc13 <5286606m@gmail.com>
2022-03-15 08:05:59 +0700

    v.04

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-a = 5
-b = 4
-print(a, b)
-a, b = b, a 
-print(a, b)
+# объявление функции
+def is_pangram(text):
+    pass
+
+# считываем данные
+text = input()
+
+# вызываем функцию
+print(is_pangram(text))
