630853149c2fc232c490bfdeed8fd1546f409384
diplodoc13 <5286606m@gmail.com>
2022-03-24 23:06:32 +0700

    V.03

--- CW.py
+++ CW.py
@@ -1,4 +1,10 @@
-def descending_order(num):
-    return int("".join(sorted(str(num), reverse=True)))
-
-print(descending_order(int(input())))
+n, m, k, x, y, z, t, a = [int(input()) for _ in range(8)]
+nm = n + m - x - t
+km = m + k - y - t
+nk = n + k - z - t
+one = (n - nm - t - nk) + (m - nm - t - km) + (k - km - t - nk)
+two = nm + km + nk
+no_one = a - one - two - t
+print(one)
+print(two)
+print(no_one)
