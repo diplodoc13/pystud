3d3b790f475f5da68cb8d65460cfcc7a2365e0e1
diplodoc13 <5286606m@gmail.com>
2022-03-31 21:34:20 +0700

    V.00

--- CW.py
+++ CW.py
@@ -1,10 +1,18 @@
-n, m, k, x, y, z, t, a = [int(input()) for _ in range(8)]
-nm = n + m - x - t
-km = m + k - y - t
-nk = n + k - z - t
-one = (n - nm - t - nk) + (m - nm - t - km) + (k - km - t - nk)
-two = nm + km + nk
-no_one = a - one - two - t
-print(one)
-print(two)
-print(no_one)
+import turtle as t
+from math import ceil
+from random import randrange
+
+t.ht()
+angle = float(input('Angle: '))
+radius = float(input('Radius: '))
+size = ceil(360 / angle)
+end = randrange(16777216)
+step = randrange(-200, 200)
+t.speed(0)
+for i in range(size):
+    t.color(hex(end - i * step).replace('0x', '#'))
+    t.circle(radius)
+    t.lt(angle)
+    if not i % step:
+        end = randrange(16777216)
+        step = randrange(-200, 200)
